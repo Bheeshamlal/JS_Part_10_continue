0f1676aa4adf75cf8293e4caed92e7d4580560ee
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-10-19 23:03:50 +0500

    this in Events

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let p = document.querySelector("p");
-p.addEventListener("click", function(){
-    console.log("clicked on paragraph");
-});
-
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
 let div = document.querySelector("div");
-div.addEventListener("mouseenter", function(){
-    console.log("mouse entered inside");
-})
+
+function changeColor(){
+    this.style.backgroundColor = "blue";
+}
+
+
+p.addEventListener("click", changeColor);
+div.addEventListener("mouseenter",changeColor);
+h1.addEventListener("click", changeColor);
+h3.addEventListener("click", changeColor);
 
