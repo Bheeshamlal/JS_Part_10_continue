b995590d779b99848f6a61866aec72cb683a6d62
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-10-19 18:52:35 +0500

    Events with other elements

---
+++ app.js
@@ -0,0 +1,10 @@
+let p = document.querySelector("p");
+p.addEventListener("click", function(){
+    console.log("clicked on paragraph");
+});
+
+let div = document.querySelector("div");
+div.addEventListener("mouseenter", function(){
+    console.log("mouse entered inside");
+})
+
