e5a2861c627a411c16db997afc60c0875c64d2e6
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-10-27 12:20:46 +0500

    keyboard events

--- app.js
+++ app.js
@@ -1,15 +1,63 @@
-let p = document.querySelector("p");
-let h1 = document.querySelector("h1");
-let h3 = document.querySelector("h3");
-let div = document.querySelector("div");
+// Keyboard Events 
+// when we print event which is default in callback function parameter 
 
-function changeColor(){
-    this.style.backgroundColor = "blue";
-}
+let inp = document.querySelector("input");
+
+inp.addEventListener("keydown", function(e){
+    console.log("key was pressed");
+    console.log(e);  // when we open this in console we will see main things like "code" and "key"
+    console.log(e.key);
+    console.log(e.code);
+    
+})
 
 
-p.addEventListener("click", changeColor);
-div.addEventListener("mouseenter",changeColor);
-h1.addEventListener("click", changeColor);
-h3.addEventListener("click", changeColor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(e){
+    
+//     console.log("clicked");
+//     this.style.backgroundColor = "blue";
+//     console.log(e);
+// });
+
+
+
+
+// let h1 = document.querySelector("h1");
+// let h3 = document.querySelector("h3");
+// let div = document.querySelector("div");
+
+
+
+// p.addEventListener("click", changeColor);
+// div.addEventListener("mouseenter",changeColor);
+// h1.addEventListener("click", changeColor);
+// h3.addEventListener("click", changeColor);
 
